09f7d1fd1894b58bba258596288c3483e4af68d1
m4yk0n <maykon.rodrigues@sptech.school>
2025-03-27 16:42:41 -0300

    fix: tabela criada por script a partir de json

--- public/script.js
+++ public/script.js
@@ -54,32 +54,56 @@ fetch("./data.json")
 .catch(error => console.error("Erro ao carregar JSON:", error));
 
 function construirTabela(dadosProdutos) {
+    // console.log("JSON: ", dadosProdutos)
     const cabecalhoTabela = document.createElement("tr");
     cabecalhoTabela.classList.add("header");
     
     const chaves = Object.keys(dadosProdutos[0]); 
         chaves.forEach(chave => {
             const itensCabecalho = document.createElement("th");
-            itensCabecalho.innerText = chave
             itensCabecalho.scope = "col";
+            if(chave == 'id') {
+                itensCabecalho.innerText = 'status'
+            } else {
+                itensCabecalho.innerText = chave
+            }
             cabecalhoTabela.appendChild(itensCabecalho);
         });
 
-    console.log(cabecalhoTabela);
+    // console.log(cabecalhoTabela);
     tabelaItens.appendChild(cabecalhoTabela);
-     
+    
+    inserirDados(dadosProdutos, tabelaItens);
 
+}
 
+function inserirDados(dadosProdutos, tabelaItens) {
 
-//   const linhaItem = document.createElement("tr");
-//   linhaItem.classList.add("lista-item");
+    dadosProdutos.forEach(produto => {
+        const linhaItem = document.createElement("tr");
+        linhaItem.classList.add("lista-item");
+        // console.log(produto)
+        const dados = Object.values(produto)
+        dados.forEach((valor, chave) => {
+            // console.log(valor, chave);
+            const infoItem = document.createElement("td");
+            infoItem.classList.add("item");
+            if(chave == 9) {
+                const botaoEditar = document.createElement("button");
+                const botaoDeletar = document.createElement("button");
+                botaoEditar.classList.add("item");
+                botaoDeletar.classList.add("item");
+                botaoEditar.innerText = '✏️';
+                botaoDeletar.innerText = '🗑️';
+                infoItem.appendChild(botaoEditar);
+                infoItem.appendChild(botaoDeletar);
+            } else {
+                infoItem.innerText = valor;      
+            }
+            linhaItem.appendChild(infoItem);  
+        })
+        tabelaItens.appendChild(linhaItem);
+        // console.log(tabelaItens)
+    })
 
-//   const infoItem = document.createElement("td");
-//   infoItem.classList.add("item");
-
-//   const botaoEditar = document.createElement("button");
-//   const botaoDeletar = document.createElement("button");
-
-//   botaoEditar.classList.add("item");
-//   botaoDeletar.classList.add("item");
 }
